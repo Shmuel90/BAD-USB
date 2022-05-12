fb1b6e31034daf8c4a0bac3617964d1af17e74be
Static <jensenmiller@unomaha.edu>
2022-05-12 17:03:35 -0500

    v1 install

---
+++ Install/1.0_contents/Cap.py
@@ -0,0 +1,16 @@
+from dictionary import buildCap
+from tkinter import *
+from tkinter import ttk
+
+def deliver_cap():
+    cap = buildCap()
+    print(cap)
+    answer = input('Enter the words above:')
+    if answer == cap:
+        exit()
+    else:
+        print('The device that was just plugged in may be a milicious device, or the words were typed incorrectly. Please ensure the device is what it\'s meant to be.')
+        deliver_cap()
+
+deliver_cap()
+
